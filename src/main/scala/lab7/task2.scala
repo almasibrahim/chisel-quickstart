743b04b88280f38c75ce23607c624613ba9b23ab
Almas Ibrahim <46762508+almasibrahim@users.noreply.github.com>
2021-04-16 04:29:40 +0500

    Add files via upload

---
+++ src/main/scala/lab7/task2.scala
@@ -0,0 +1,46 @@
+package lab7
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.ChiselEnum
+
+class task2 extends Module {
+    val io = IO (new Bundle {
+        val in = Input ( UInt (1. W ) )
+        val start = Input ( Bool () )
+        val out = Output ( UInt (8. W ) )
+        val flag = Output ( UInt (1. W ) )
+    })
+    val s0 :: s1 :: Nil = Enum(2)
+    val state = RegInit(s0)
+    val register = RegInit(0.U(8.W))
+    io.out:=0.U
+    io.flag:=0.U
+    switch (state){
+        is (s0){
+            when (io.start===1.U){
+		state:=s1
+	    }.otherwise{
+		io.out:=register
+            	io.flag:=0.U
+	    }
+		
+        }
+        is (s1){
+	    when (io.start===0.U){
+		state:=s0
+	    }.otherwise{
+		    when(io.in === 1.U){
+				register:= register << 1.U | io.in
+				io.out:=register
+				io.flag:=1.U
+
+		    }.otherwise{
+				register:= register << 1.U
+				io.out := register
+				io.flag:=1.U
+		    }
+	    }
+        }
+    }
+}
